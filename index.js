08413b6528a9c4a2e21f1c8edf6b1e265a46d193
SIDDHARTH RAMESH CHAND RAJA <45534008+siddharthrajaraja@users.noreply.github.com>
2019-10-24 22:41:27 +0530

    look for abc file in DBMS

---
+++ index.js
@@ -0,0 +1,46 @@
+var express =require('express')
+var fs =require('fs')
+const path=require('path');
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+
+const app=express()
+
+
+app.set('view engine','ejs');
+app.use('/assets',express.static('assets'));
+
+
+app.get('/get',(req,res)=>{
+    res.render('select')
+})
+
+app.post('/submit',urlencodedParser,(req,res)=>{
+    console.log(req.body)
+    var pathname=__dirname+'/experiments/'+req.body.direc+"/"+req.body.fname+'.txt'
+
+    const lineByLine = require('n-readlines');
+const liner = new lineByLine(pathname);
+ 
+let line;
+let lineNumber = 0;
+
+var output=[]
+ 
+while (line = liner.next()) {
+            output.push(line.toString('ascii'))
+}
+ 
+    res.render('output',{data:output})
+console.log('end of line reached');
+
+    /*fs.readFile(__dirname+'/experiments/'+req.body.direc+"/"+req.body.fname+'.txt','utf8',(err,data)=>{
+        if(err) throw err;
+    res.send(data)
+
+    })*/
+});
+
+
+app.listen(1234)
